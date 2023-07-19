0c623b0c5a2a6960afd7968733c73e3131d5fdee
Alder Pinto <alder.pinto@gmail.com>
2023-07-19 14:25:47 -0300

    nEW vERSION 7.0.8

--- docker/run.sh
+++ docker/run.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
+VOLUME_DIR=${OTRS_ROOT:-/opt/znuny}
 
 function prepare_znuny () {
     echo "Preparing znuny..."
-    cp -rfp /app-files/znuny/* ${OTRS_ROOT}
-    su -c "${OTRS_ROOT}bin/znuny.Console.pl Maint::Config::Rebuild" -s /bin/bash znuny
-    su -c "${OTRS_ROOT}bin/znuny.Console.pl Maint::Cache::Delete" -s /bin/bash znuny
-    ${OTRS_ROOT}bin/znuny.SetPermissions.pl --znuny-user=znuny --web-group=nginx ${OTRS_ROOT}
+    cp -rfp /app-files/znuny/* ${OTRS_ROOT}/
+    su -c "${OTRS_ROOT}/bin/znuny.Console.pl Maint::Config::Rebuild" -s /bin/bash znuny
+    su -c "${OTRS_ROOT}/bin/znuny.Console.pl Maint::Cache::Delete" -s /bin/bash znuny
+    ${OTRS_ROOT}/bin/znuny.SetPermissions.pl --znuny-user=znuny --web-group=nginx ${OTRS_ROOT}/
     rm -fr /app-files/znuny   
 }
-
-VOLUME_DIR=${OTRS_ROOT:-/opt/znuny/}
 
 # Verificar se o diretório do volume existe
 if [ -d "$VOLUME_DIR" ]; then
@@ -24,7 +23,7 @@ if [ -d "$VOLUME_DIR" ]; then
 else
     echo "O volume ainda não foi criado."
     # Coloque aqui o código para criar o volume e executar a configuração inicial do ambiente
-    mkdir -p ${OTRS_ROOT}
+    mkdir -p ${OTRS_ROOT}/
     prepare_znuny  
 fi
 
