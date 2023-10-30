162c575f530a4c99adb88b32f8194b727ae17c3a
Alder Pinto <alder.pinto@gmail.com>
2023-10-30 14:29:53 -0300

    otrs 6.0.30

--- docker/init/web.psgi
+++ docker/init/web.psgi
@@ -2,16 +2,16 @@ use Plack::App::CGIBin;
 use Plack::Builder;
 use HTTP::Server::PSGI;
 use Plack::App::File;
-use lib "/opt/znuny";
-use lib "/opt/znuny/Kernel/cpan-lib";
+use lib "/opt/otrs";
+use lib "/opt/otrs/Kernel/cpan-lib";
 
 my $app = Plack::App::CGIBin->new(
-    root => "/opt/znuny/bin/cgi-bin",
+    root => "/opt/otrs/bin/cgi-bin",
     exec_cb => sub { 1 }
     )->to_app;
-my $app_web = Plack::App::File->new(root => "/opt/znuny/var/httpd/htdocs")->to_app;
+my $app_web = Plack::App::File->new(root => "/opt/otrs/var/httpd/htdocs")->to_app;
 builder {
       enable "StackTrace", force => $ENV{DEBUG_MODE};
-      mount "/znuny" => $app;
-      mount "/znuny-web" => $app_web;
+      mount "/otrs" => $app;
+      mount "/otrs-web" => $app_web;
 };
--- docker/run.sh
+++ docker/run.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-function prepare_znuny () {
-    echo "Preparing znuny..."
-    cp -rfp /app-files/znuny/* ${OTRS_ROOT}
-    su -c "${OTRS_ROOT}bin/znuny.Console.pl Maint::Config::Rebuild" -s /bin/bash znuny
-    su -c "${OTRS_ROOT}bin/znuny.Console.pl Maint::Cache::Delete" -s /bin/bash znuny
-    ${OTRS_ROOT}bin/znuny.SetPermissions.pl --znuny-user=znuny --web-group=nginx ${OTRS_ROOT}
-    rm -fr /app-files/znuny   
+function prepare_otrs () {
+    echo "Preparing otrs..."
+    cp -rfp /app-files/otrs/* ${OTRS_ROOT}
+    su -c "${OTRS_ROOT}bin/otrs.Console.pl Maint::Config::Rebuild" -s /bin/bash otrs
+    su -c "${OTRS_ROOT}bin/otrs.Console.pl Maint::Cache::Delete" -s /bin/bash otrs
+    ${OTRS_ROOT}bin/otrs.SetPermissions.pl --otrs-user=otrs --web-group=nginx ${OTRS_ROOT}
+    rm -fr /app-files/otrs   
 }
 
-VOLUME_DIR=${OTRS_ROOT:-/opt/znuny/}
+VOLUME_DIR=${OTRS_ROOT:-/opt/otrs/}
 
 # Verificar se o diretório do volume existe
 if [ -d "$VOLUME_DIR" ]; then
@@ -19,13 +19,13 @@ if [ -d "$VOLUME_DIR" ]; then
     else
         echo "O ambiente ainda não foi configurado."
         # Coloque aqui o código para executar a configuração inicial do ambiente
-        prepare_znuny     
+        prepare_otrs     
     fi
 else
     echo "O volume ainda não foi criado."
     # Coloque aqui o código para criar o volume e executar a configuração inicial do ambiente
     mkdir -p ${OTRS_ROOT}
-    prepare_znuny  
+    prepare_otrs  
 fi
 
 /usr/bin/supervisord -c /etc/supervisord.conf&
