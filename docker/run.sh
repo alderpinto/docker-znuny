47a22c0bc493b62228c12ab95a2dbbb39121446a
Alder Pinto <alder.pinto@gmail.com>
2024-03-04 16:37:53 -0300

    6.5.6 Completo

--- docker/run.sh
+++ docker/run.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
+
+. ./functions.sh
+
 VOLUME_DIR=${ZNUNY_ROOT:-/opt/otrs}
 
-function prepare_znuny () {
-    echo "Preparing znuny..."
-    cp -rfp /app-files/otrs/* ${ZNUNY_ROOT}/
-    su -c "${ZNUNY_ROOT}/bin/otrs.Console.pl Maint::Config::Rebuild" -s /bin/bash otrs
-    su -c "${ZNUNY_ROOT}/bin/otrs.Console.pl Maint::Cache::Delete" -s /bin/bash otrs
-    ${ZNUNY_ROOT}/bin/otrs.SetPermissions.pl --otrs-user=otrs --web-group=nginx ${ZNUNY_ROOT}/
-    rm -fr /app-files/otrs   
-}
+wait_for_db
 
 # Verificar se o diretório do volume existe
 if [ -d "$VOLUME_DIR" ]; then
     # Verificar se existem arquivos ou diretórios dentro do diretório do volume
     if [ "$(ls -A $VOLUME_DIR)" ]; then
-        echo "O ambiente já foi configurado anteriormente."
+        print_info "O ambiente já foi configurado anteriormente."
     else
-        echo "O ambiente ainda não foi configurado."
+        print_info "O ambiente ainda não foi configurado."
         # Coloque aqui o código para executar a configuração inicial do ambiente
-        prepare_znuny     
+        print_info "Starting \e[${ZNUNY_ASCII_COLOR_BLUE}m ((OTRS))\e[0m \e[31m${ZNUNY_VERSION}\e[0m \e[${ZNUNY_ASCII_COLOR_BLUE}mCommunity Edition\e[0m \e[0m\n"
+        if [ -e "${ZNUNY_CONFIG_MOUNT_DIR}/var/tmp/firsttime" ]; then
+            #Load default install
+            load_defaults
+            #Set default admin user password
+            print_info "Setting password for default admin account \e[${ZNUNY_ASCII_COLOR_BLUE}mroot@localhost\e[0m to: \e[31m**********\e[0m"
+            su -c "${ZNUNY_ROOT}/bin/otrs.Console.pl Admin::User::SetPassword root@localhost ${ZNUNY_ROOT_PASSWORD}" -s /bin/bash otrs
+            rm -fr ${ZNUNY_ROOT}/var/tmp/firsttime
+        fi          
     fi
-else
-    echo "O volume ainda não foi criado."
-    # Coloque aqui o código para criar o volume e executar a configuração inicial do ambiente
-    mkdir -p ${ZNUNY_ROOT}/
-    prepare_znuny  
+    install_modules ${ZNUNY_ADDONS_PATH}
 fi
 
 /usr/bin/supervisord -c /etc/supervisord.conf&
